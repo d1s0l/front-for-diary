8eb43aa19eda61f0ad2137a78239fc84b64091ff
d1s0l <d.solovyov214@gmail.com>
2025-04-10 00:56:00 +0300

    not_found_404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,14 @@
 export default function NotFound() {
     return(
-        <div>404</div>
+        <div className="min-h-screen flex flex-col justify-center items-center bg-white">
+            <div className="text-center">
+                <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-8xl text-violet-900 font-extrabold mb-4 sm:mb-8">
+                    not found page
+                </h1>
+                <p className="text-5xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-8xl text-violet-900 font-extrabold">
+                    404
+                </p>
+            </div>
+        </div>
     )
 }
