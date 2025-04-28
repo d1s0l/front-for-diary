84d8cd03dc7ca674db77b42c8cad5d2ac9b34935
d1s0l <d.solovyov214@gmail.com>
2025-04-29 00:52:48 +0300

    feat: Добавлен новый favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from 'next/font/google';
 
@@ -8,6 +7,11 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: "Электронный дневник",
   description: "Электронный школьный дневник",
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
